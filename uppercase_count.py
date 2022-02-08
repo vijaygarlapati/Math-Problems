90d04cc9c44501d079c3930694bbc53ce311b0db
VIJAYKUMAR GARLAPATI <vijayg.engineer@gmail.com>
2022-02-08 21:35:16 +0530

    Update uppercase_count.py

--- uppercase_count.py
+++ uppercase_count.py
@@ -1,11 +1,19 @@
 def upp_letters(text):
-    '''Count the number of supper case starting letters in each word of the sentence'''
-    c=0
-    l=list(text.split())
-    for w in l:
+    '''Count the number of uppercase starting letters and lowercase letters in each word of the sentence'''
+    u=0
+    l=0
+    word_list=list(text.split())
+    for w in word_list:
         if w[0].isupper()==True:
-            c = c+1
+            u = u+1
         else:
             pass
-    return c
+    for w in word_list:
+        for i in range(0,len(w)):
+            if w[i].islower()==True:
+                l=l+1
+            else:
+                pass
+    print('No of uppercase letters: ',u)
+    print('No of uppercase letters: ',l)
             
