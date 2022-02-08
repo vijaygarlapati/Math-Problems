895488173c3d6d988eacdce07ecee8edf5513f00
VIJAYKUMAR GARLAPATI <vijayg.engineer@gmail.com>
2022-02-08 16:26:29 +0530

    Update spy_game.py

--- spy_game.py
+++ spy_game.py
@@ -1,10 +1,8 @@
 def spy_game(list):
     '''Spy game: write a function that takes a list of integers and returns True if
     it contains 007 in order'''
-
-    for num in range(0,len(list)-2):
-        if list[num]==0 and list[num+1]==0 and list[num+2]==7:
-            return True
-        else:
-            pass
-    return False
+    code = [0,0,7,'x']
+    for num in list:
+        if num == code[0]:
+            code.pop(0)
+    return len(code)==1
