308d148c4b92791ae8858275f2015d2bb0a1450a
VIJAYKUMAR GARLAPATI <vijayg.engineer@gmail.com>
2022-02-11 16:14:21 +0530

    Update oopsprob1.py

--- oopsprob1.py
+++ oopsprob1.py
@@ -1,17 +1,20 @@
 #Problem 1
 #Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
 
-class line():
+class Line():
     
-    def __init__(self,x1,y1,x2,y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+    def __init__(self,cord1,cord2):
+        self.cord1 = cord1
+        self.cord2 = cord2
+        
     def distance(self):
         from math import sqrt
-        return sqrt((self.x2-self.x1)**2 + (self.y2-self.y1)**2)
+        x1,y1 = self.cord1
+        x2,y2 = self.cord2
+        return sqrt((x2-x1)**2 + (y2-y1)**2)
     
     def slope(self):
-        return (self.y2-self.y1)/(self.x2-self.x1)
+        x1,y1 = self.cord1
+        x2,y2 = self.cord2
+        return (y2-y1)/(x2-x1)
       
